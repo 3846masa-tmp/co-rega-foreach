b9b92aba62fa6649a92cab4fe57ce79c4b4aba0a
3846masa <3846masahiro+git@gmail.com>
2015-07-24 19:16:34 +0000

    First commit.

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # co-foreach
 Run generator function as Array.prototype.forEach callback.
+
+## Usage
+
+### Basic
+```js
+'use strict';
+var co = require('co');
+var co_foreach = require('co-foreach');
+
+// If callback is normal func, run with original forEach.
+[1, 2, 3].forEach(function(num) {
+  console.log('Number is %d.', num);
+});
+
+co(function*() {
+  var request = require('co-request');
+
+  // If callback is generator func, run with co-foreach.
+  yield [1, 2, 3].forEach(function*(num) {
+    console.log('Number is %d.', num);
+    var res = request.get('http://example.com/#' + num);
+    console.log('StatusCode : %d', res.statusCode);
+  });
+  process.exit();
+});
+```
+
+### Switch between original and co-foreach
+```js
+'use strict';
+var co = require('co');
+
+// co-foreach is enabled.
+var co_foreach = require('co-foreach');
+
+// co-foreach is disabled.
+co_foreach.disabled();
+
+// co-foreach is enabled.
+co_foreach.enabled();
+```
---
+++ index.js
@@ -0,0 +1,51 @@
+'use strict';
+var co = require('co');
+var isGeneratorFn = require('is-generator').fn;
+
+var __forEach = Array.prototype.forEach;
+var __coForEach = co.wrap(function*( callback, thisArg ) {
+  // Production steps of ECMA-262, Edition 5, 15.4.4.18
+  // Reference: http://es5.github.com/#x15.4.4.18
+  // Based from MDN
+  var T, k = 0;
+  if ( this == null ) {
+    throw new TypeError( " this is null or not defined" );
+  }
+  var O = Object(this);
+  var len = O.length >>> 0;
+  if ( {}.toString.call(callback) != "[object Function]" ) {
+    throw new TypeError( callback + " is not a function" );
+  }
+  if ( thisArg ) T = thisArg;
+  while( k < len ) {
+    var kValue;
+    if ( k in O ) {
+      kValue = O[ k ];
+      yield co.wrap( callback ).call( T, kValue, k, O );
+    }
+    k++;
+  }
+});
+
+var enabledCoForEach = function(Array) {
+  if (Array.prototype.forEach !== __forEach) return;
+  Array.prototype.forEach = function(fn) {
+    if (isGeneratorFn(fn)) {
+      return __coForEach.apply(this, arguments);
+    } else {
+      __forEach.apply(this, arguments);
+    }
+  };
+};
+
+var disabledCoForEach = function(Array) {
+  if (Array.prototype.forEach !== __forEach) {
+    Array.prototype.forEach = __forEach;
+  }
+};
+
+module.exports = {
+  disabled : disabledCoForEach,
+  enabled : enabledCoForEach
+};
+enabledCoForEach(Array);
